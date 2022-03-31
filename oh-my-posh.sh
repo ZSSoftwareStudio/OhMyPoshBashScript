02f0384790de889f77d9a59a345347a536394547
Zarifprogrammer <56337492+ZarifSoftware@users.noreply.github.com>
2022-03-31 15:39:03 +0600

    Add files via upload

---
+++ oh-my-posh.sh
@@ -0,0 +1,20 @@
+## Install Oh my Posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+echo "Zarifprogrammer: Successfully downloaded Oh My Posh"
+
+sleep 2
+
+## Download the themes
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+
+echo "Zarifprogrammer: All Themes are extracted"
+
+sleep 2
+
+echo "Zarifprogrammer: Successfully Installed Oh My Posh"
